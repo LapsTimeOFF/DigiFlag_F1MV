5052a91d385c719bf99dfc3381d5468d3cecf9c6
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-08-31 19:03:28 +0200

    Updating for the building

--- build_prod.sh
+++ build_prod.sh
@@ -7,24 +7,25 @@ echo 'Old builds removed'
 echo 'Starting building...'
 echo 'Building Win32x64'
 npx electron-forge package --arch=x64 --platform=win32
-mv out/* ../*
-mv ./Can\'tBeOpenBug.sh ../
-rm -rf out
 echo 'Building Win32x64 completed'
 echo 'Building Darwin'
 npx electron-forge package --platform=darwin
 echo 'Building Darwin completed'
 echo "Making can't be open file"
-echo "sudo spctl --master-disable" > Can\'tBeOpenBug.sh
-echo "sudo chmod -R 777 /Applications/F1MV-DigiFlag.app" > Can\'tBeOpenBug.sh
-echo "xattr -d com.apple.quarantine /Applications/F1MV-DigiFlag.app" > Can\'tBeOpenBug.sh
-echo "xattr -cr /Applications/F1MV-DigiFlag.app" > Can\'tBeOpenBug.sh
-echo "" > Can\'tBeOpenBug.sh
-echo "echo 'Done.'" > Can\'tBeOpenBug.sh
+echo "sudo spctl --master-disable" > out/Can\'tBeOpenBug.sh
+echo "sudo chmod -R 777 /Applications/F1MV-DigiFlag.app" > out/Can\'tBeOpenBug.sh
+echo "xattr -d com.apple.quarantine /Applications/F1MV-DigiFlag.app" > out/Can\'tBeOpenBug.sh
+echo "xattr -cr /Applications/F1MV-DigiFlag.app" > out/Can\'tBeOpenBug.sh
+echo "" > out/Can\'tBeOpenBug.sh
+echo "echo 'Done.'" > out/Can\'tBeOpenBug.sh
 echo "Making can't be open file done"
 echo 'Building Linux'
 npx electron-forge package --platform=linux
 echo 'Building Linux completed'
+mv out/* ../*
+mv ./Can\'tBeOpenBug.sh ../
+rm -rf out
+cd ..
 echo 'Removing useless files...'
 rm -rf ./F1MV-DigiFlag-darwin-arm64/LICENSE*
 rm -rf ./F1MV-DigiFlag-darwin-arm64/version
