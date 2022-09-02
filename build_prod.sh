0553e2472bc6c1bf322e7f8f784c754829cb2377
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-09-02 18:29:18 +0200

    Some updates

--- build_prod.sh
+++ build_prod.sh
@@ -8,14 +8,6 @@ echo 'Building Win32x64 completed'
 echo 'Building Darwin'
 electron-packager ./ F1MV-DigiFlag --platform=darwin
 echo 'Building Darwin completed'
-echo "Making can't be open file"
-echo "sudo spctl --master-disable" > out/Can\'tBeOpenBug.sh
-echo "sudo chmod -R 777 /Applications/F1MV-DigiFlag.app" > out/Can\'tBeOpenBug.sh
-echo "xattr -d com.apple.quarantine /Applications/F1MV-DigiFlag.app" > out/Can\'tBeOpenBug.sh
-echo "xattr -cr /Applications/F1MV-DigiFlag.app" > out/Can\'tBeOpenBug.sh
-echo "" > out/Can\'tBeOpenBug.sh
-echo "echo 'Done.'" > out/Can\'tBeOpenBug.sh
-echo "Making can't be open file done"
 echo 'Building Linux'
 electron-packager ./ F1MV-DigiFlag --platform=linux
 echo 'Building Linux completed'
@@ -24,8 +16,8 @@ rm -rf ./F1MV-DigiFlag-darwin-arm64/LICENSE*
 rm -rf ./F1MV-DigiFlag-darwin-arm64/version
 echo 'Useless files removed'
 echo 'Zipping files...'
-zip F1MV-DigiFlag-linux-arm64.zip ./F1MV-DigiFlag-linux-arm64/*
-zip F1MV-DigiFlag-win32-x64.zip ./F1MV-DigiFlag-win32-x64/*
+zip F1MV-DigiFlag-linux-arm64.zip ./F1MV-DigiFlag-linux-arm64/* -r
+zip F1MV-DigiFlag-win32-x64.zip ./F1MV-DigiFlag-win32-x64/* -r
 echo 'Zippied files done'
 echo 'Create Applications folder alias'
 ln -s /Applications ./F1MV-DigiFlag-darwin-arm64/Applications
