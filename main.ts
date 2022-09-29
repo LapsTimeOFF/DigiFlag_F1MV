afcefe5910f5e5089556490bf714433ac1326b34
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-09-29 18:40:22 +0200

    Adding Pixoo64

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ expressApp.get('/getGif/:gif/:themeID', (req:any,res:any) => {
     let theme:any = themes[themeID]
     let gifPath:any = theme.gifs[gif];
 
-    res.send(gifPath)
+    res.sendFile(`${__dirname}/${gifPath}`)
 })
 
 expressApp.get('/getGifPixoo/:gif/:themeID', (req:any,res:any) => {
@@ -29,7 +29,7 @@ expressApp.get('/getGifPixoo/:gif/:themeID', (req:any,res:any) => {
     }
     let gifPath:any = theme.gifs.pixoo64[gif];
 
-    res.send(gifPath)
+    res.sendFile(`${__dirname}/${gifPath}`)
 })
 
 expressApp.get('/pixoo/:gif/:themeID/:ip', (req:any,res:any) => {
@@ -40,13 +40,12 @@ expressApp.get('/pixoo/:gif/:themeID/:ip', (req:any,res:any) => {
         res.send('Theme requested doesn\'t support Pixoo64')
         return;
     }
-    let jsonRequest = {
+
+    request.post(`http://${ip}:80/post`, { json: {
         "Command": "Device/PlayTFGif",
         "FileType": 2,
-        "FileName": `http://${address}:9093/getGifPixoo/${gif}/${themeID}`
-    }
-
-    request.post(`http://${ip}:80/post`, { json: jsonRequest }, (err: any, resp: any, body: any) => {
+        "FileName": `http://${address()}:9093/getGifPixoo/${gif}/${themeID}`
+    } }, (err: any, res: any, body: any) => {
         let response = JSON.parse(body)
         if(err || response.error_code !== 0) {
             res.statusCode = 500
