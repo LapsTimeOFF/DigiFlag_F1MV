e00a84ea9a83355ae13dcdd71d92ba82d0db7829
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-10-23 17:17:19 +0200

    Updating JSONs and Scripts

--- build_prod.sh
+++ build_prod.sh
@@ -6,13 +6,13 @@ tsc
 echo 'Building .ts file done'
 echo 'Starting building...'
 echo 'Building Win32x64'
-electron-packager ./ F1MV-DigiFlag --platform=win32 --arch=x64
+electron-packager ./ F1MV-DigiFlag --platform=win32 --arch=x64 --icon=assets/logos/DigiFlag.png
 echo 'Building Win32x64 completed'
 echo 'Building Darwin'
-electron-packager ./ F1MV-DigiFlag --platform=darwin
+electron-packager ./ F1MV-DigiFlag --platform=darwin --icon=assets/logos/DigiFlag.png
 echo 'Building Darwin completed'
 echo 'Building Linux'
-electron-packager ./ F1MV-DigiFlag --platform=linux
+electron-packager ./ F1MV-DigiFlag --platform=linux --icon=assets/logos/DigiFlag.png
 echo 'Building Linux completed'
 echo 'Removing useless files...'
 rm -rf ./F1MV-DigiFlag-darwin-arm64/LICENSE*
