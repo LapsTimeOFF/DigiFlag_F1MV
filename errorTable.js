e450395a07b5db70bf98ce473bd753a9940557be
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-10-26 17:36:26 +0200

    Adding an error table

---
+++ errorTable.js
@@ -0,0 +1,4 @@
+module.export = {
+    failedToCreateNewInstance: new Error('An error occurred during the creation of the instance.'),
+    failedToLoadNewInstance: new Error('You can\'t run more than 1 instance of the DigiFlag. Please use the "Create a new instance" button')
+}
