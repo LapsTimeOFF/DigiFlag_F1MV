a01ad01ed43fea80d31e1bea69da02a5ca99bf57
Jared Provencio <11319293+TheBaconCat@users.noreply.github.com>
2023-02-16 15:35:44 -0700

    Initial Commit Of DigiFlag Web

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  base: "/DigiFlag_F1MV/",
+  build:{
+    minify:true
+  }
+  }
+)
